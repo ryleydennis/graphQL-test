4feb18239d317d5bd23abee69dc24641b2664e24
Ryley Dennis <ryleydennis@gmail.com>
2021-08-07 13:02:17 -0400

    Completed step 6

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from './Link';
 import { useQuery, gql } from '@apollo/client';
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   {
     feed {
       id
@@ -11,6 +11,16 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
@@ -20,16 +30,16 @@ const LinkList = () => {
   const {data} = useQuery(FEED_QUERY)
 
   return (
-      <div>
-          {data && (
-              <>
-                {data.feed.links.map((link) => (
-                    <Link key={link.id} link={link} />
-                ))}
-              </>
-          )}
-      </div>
-  )
+    <div>
+      {data && (
+        <>
+          {data.feed.links.map((link, index) => (
+            <Link key={link.id} link={link} index={index} />
+          ))}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default LinkList;
