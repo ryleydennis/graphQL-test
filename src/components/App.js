5bd0285c3ec760171b9d5147033e172da9de8622
Ryley Dennis <ryleydennis@gmail.com>
2021-08-07 12:26:55 -0400

    Routing complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React, { Component } from "react";
-import CreateLink from "./CreateLink";
-import LinkList from "./LinkList";
+import React from 'react';
+import CreateLink from './CreateLink';
+import Header from './Header';
+import LinkList from './LinkList';
+import { Switch, Route } from 'react-router-dom';
+import Login from './Login';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <CreateLink/>
-        </div>
-        <div>
-          <LinkList/>
-        </div>
+const App = () => {
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route
+            exact
+            path="/create"
+            component={CreateLink}
+          />
+          <Route exact path="/login" component={Login} />
+        </Switch>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
